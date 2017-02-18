fd1ad4b729dee75a5bd195d3546fd820ab428f0b
Anton Smolich <a.smolich@gmail.com>
2017-02-18 14:39:11 -0800

    Max of three numbers added

---
+++ 01_Basic/MaxOfThree.java
@@ -0,0 +1,9 @@
+/**
+ * Find a maximum of three given numbers.
+ */
+public class MaxOfThree {
+     public int max(int a, int b, int c) {
+        // insert your code hede
+        return 0;
+    }
+}
