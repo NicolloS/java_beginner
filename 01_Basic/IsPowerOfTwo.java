64790eeb636dd75e2ee83962da66df405e4d7406
Anton Smolich <a.smolich@gmail.com>
2017-02-18 23:19:28 -0800

    More problems to solve

---
+++ 01_Basic/IsPowerOfTwo.java
@@ -0,0 +1,9 @@
+/**
+ * Check if given number is a power of 2.
+ *
+ * E.g. 1=2^0, 2=2^1, 4=2^2, 8=2^3, 16=2^4, ...
+ */
+public class IsPowerOfTwo {
+    public boolean isPowerOfTwo(int a) {
+    }
+}
---
+++ 02_Array/SearchInArray.java
@@ -0,0 +1,7 @@
+/**
+ * Find a given element in an integer array and return its index, return -1 if there is none.
+ */
+public class SearchInArray {
+    public int search(int[] arr) {
+    }
+}
