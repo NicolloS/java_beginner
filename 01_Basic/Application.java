4619599a4543c5bea41fa2b7e3831dfd6b2ce5f2
NicolloS <smolichniko@gmail.com>
2017-02-19 22:41:31 +0300

    homework

---
+++ 01_Basic/Application.java
@@ -0,0 +1,59 @@
+package com.smolich.stringtoarray;
+
+import java.util.Random;
+
+public class Application {
+	private String templateString = "12345";
+	private int[] arr;
+	private int[][] arrzamas;
+	
+	public static void main(String[] args) {
+		Application theApp = new Application();
+		theApp.runTheApp();
+		System.out.println("max of three = ");
+		System.out.println(theApp.maxofthree(3, 5, 6));
+		theApp.inzamas();
+	}
+
+	public void runTheApp() {
+		arr = new int[templateString.length()];
+		for (int i = 0; i < arr.length; i++) {
+			char ch = templateString.charAt(i);
+			String s = String.valueOf(ch);  
+			arr[i] = Integer.parseInt(s);
+			
+		}
+		
+		for (int i : arr) {
+			System.out.println(i);
+			
+		}
+		
+	}
+	
+	public int maxofthree(int a,int b,int c){
+		if (a>b){
+			if (a>c) return a;
+			else return c;
+		}
+		else if (b>c) return b;
+		else return c;
+	}
+	public void inzamas () {
+		Random ran = new Random(System.currentTimeMillis());
+		arrzamas = new int [6][7];
+		for (int i = 0; i < arrzamas.length; i++) {
+			for (int j = 0; j < arrzamas[i].length; j++) {
+				arrzamas[i][j] = ran.nextInt(100);
+			}
+			
+		}
+		for (int[] ar : arrzamas) {
+			for (int i : ar) {
+				System.out.print(i+" ");
+			}
+			System.out.println("");
+		}
+	}
+	
+}
