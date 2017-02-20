67f971347c7dc1656e12acfbf33e470fe61a1ba7
Anton Smolich <a.smolich@gmail.com>
2017-02-20 09:53:35 -0800

    Added matrix tasks

---
+++ 03_ArrayOfArrays/MatrixSpiral.java
@@ -0,0 +1,13 @@
+/**
+ * Given a number n &gt; 0. Create a matrix nxn and fill it in with numbers from 1 to nxn as a spiral like the following.
+ * E.g. for n = 5, the matrix would look like
+ * [ 1  2  3  4 5]
+ * [16 17 18 19 6]
+ * [15 24 25 20 7]
+ * [14 23 22 21 8]
+ * [13 12 11 10 9]
+ */
+public class MatrixSpiral {
+
+}
+
