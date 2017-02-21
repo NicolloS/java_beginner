5381b075fb60b738f46bbfdb5c33ae8b4480e492
NicolloS <smolichniko@gmail.com>
2017-02-22 01:02:37 +0300

    3 methods

--- 02_Array/ReverseArray.java
+++ 02_Array/ReverseArray.java
@@ -5,11 +5,11 @@
  */
 public class ReverseArray {
 	public static void main(String[] arr) {
-		int[] array = new int[] { 1, 2, 3, 4, 5 };
+		int[] array = new int[] { 1, 2, 3, 4, 5,6 };
 		int[] array1 = new int[] { 100, -22, 43, 14, 11, 55, 101, 202 };
 		ReverseArray revArr = new ReverseArray();
-
-		int[] arrayRev = revArr.reverseArrInt(array);
+		revArr.print(array);
+		int[] arrayRev = revArr.reverseArrIntBuff(array);
 		System.out.println("исходный массив");
 		revArr.print(array);
 		System.out.println("получившийся массив");
@@ -29,16 +29,37 @@ public class ReverseArray {
 	}
 
 	// dopisat' mass
+	// [13,45,56,9,14,-4,67,0,-7]
 	public int[] reverseArrInt(int[] arr) {
-		int[] arr2 = arr;
+		int[] arr2 = new int[arr.length];
 		int j = 0;
-		for (int i = arr.length; j <arr.length; i--,j++) {
+		for (int i = arr.length-1; j <arr.length; i--,j++) {
+			//i = 8; j = 0; arr[i] = -7 ;arr2[j] = 13 => -7;
+			//i = 7; j = 1; arr[i] = 0; arr2[j] = 45 =>0; 
+			//i = 6; j = 2; arr[i] = 67; arr2[j] = 56 =>67;
+			//i = 5; j = 3; arr[i] = -4; arr2[j] = 9 =>-4;
+			//i = 4; j = 4; arr[i] = 14; arr2[j] = 14 =>14;
+			//i = 3; j = 5; arr[i] = 9; arr2[j] = -4 =>9;
+			//i = 2; j = 6; arr[i] = 56; arr2[j] = 67 =>0;
+			//i = 1; j = 7; arr[i] = ; arr2[j] = 0 =>0;
+			//i = 0; j = 8; arr[i] = 0; arr2[j] = -7 =>0;
+			//i = arr.length-j;
 			arr2[j] = arr[i]; //rugaetsya :-(
 			
 		}
 		return arr2;
 	}
-
+	public int[] reverseArrIntBuff(int[] arr) {
+		//int[] arr2 = arr;
+		int j = 0,buff;
+		for (int i = arr.length-1; j <arr.length/2; i--,j++) {
+			buff = arr[j];
+			arr[j] = arr[i];
+			arr[i] = buff;
+		}
+		return arr;
+	}
+	
 	public void reverseArrVoid(int[] arr) {
 		for (int i = 0; i < arr.length - 1; i++) {
 			for (int j = 0; j < arr.length - i - 1; j++) {
