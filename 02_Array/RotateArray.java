6a181a7ba25da30dad0f3bee4e7e27b400e4dd53
NicolloS <smolichniko@gmail.com>
2017-02-24 02:14:33 +0300

    it allmost done

--- 02_Array/RotateArray.java
+++ 02_Array/RotateArray.java
@@ -4,11 +4,44 @@
  * E.g.
  * A : [1 2 3 4 5 6]
  * K : 1
+ * K2 :3
  *
  * The output :
  * [2 3 4 5 6 1]
+ * [4 5 6 3 2 1] // [4 5 6 1 2 3]??
  */
 public class RotateArray {
-    public int[] rotate(int[] arr) {
-    }
+	public static void main(String[] arr) {
+		int[] array = new int[] { 1, 2, 3, 4, 5, 6 };
+		int[] array1 = new int[] { 1, 2, 3, 4, 5, 6 };
+		RotateArray rotArr = new RotateArray();
+		ReverseArray revArr = new ReverseArray();
+		revArr.print(array);
+		rotArr.rotate(array);
+		revArr.print(array);
+		rotArr.rotateN(array1,3);
+		revArr.print(array1);
+	}
+
+	public int[] rotate(int[] arr) {
+		int buf;
+		for (int i = 0; i < arr.length - 1; i++) {
+			buf = arr[i];
+			arr[i] = arr[i + 1];
+			arr[i + 1] = buf;
+		}
+		return arr;
+	}
+
+	public int[] rotateN(int[] arr, int n) {
+		int buf;
+		for (int j = 0; j < n; j++) {
+			for (int i = 0; i < arr.length - 1; i++) {
+				buf = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = buf;
+			}
+		}
+		return arr;
+	}
 }
