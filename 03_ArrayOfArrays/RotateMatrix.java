e6a92075dab4e13cfe162d530e53caa3c3d722ff
Anton Smolich <a.smolich@gmail.com>
2017-02-26 13:48:35 -0800

    Rotate Matrix task added

---
+++ 03_ArrayOfArrays/RotateMatrix.java
@@ -0,0 +1,26 @@
+/*
+ * Rotate Matrix.
+ *
+ * Given a n*n matrix representing an image.
+ *
+ * Rotate the image by 90 degrees (clockwise).
+ *
+ * You need to do this in place.
+ *
+ * Example:
+ * If the array is
+ * [
+ *   [1, 2],
+ *   [3, 4]
+ * ]
+ *
+ * Then the rotated array becomes:
+ * [
+ *   [3, 1],
+ *   [4, 2]
+ * ]
+ */
+public class RotateMatrix {
+    public void rotate(int[][] a) {
+    }
+}
