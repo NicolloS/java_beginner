5385c10ce90e7faf51c000d094c9ad15e894102b
NicolloS <smolichniko@gmail.com>
2017-02-28 00:15:48 +0300

    it done :(

--- 03_ArrayOfArrays/MatrixTriangle.java
+++ 03_ArrayOfArrays/MatrixTriangle.java
@@ -1,27 +1,35 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-/**
+/*
  * Given a number n. Initialize and print triangular matrix that looks like
- * this: [1, 2, 3, 4, ..., n] [2, 3, 4, ..., n] [3, 4, ..., n] ... [(n-1), n]
+ * this: 
+ * [1, 2, 3, 4, ..., n] 
+ * [2, 3, 4, ..., n] 
+ * [3, 4, ..., n] 
+ * ... 
+ * [(n-1), n]
  * [n]
  */
 public class MatrixTriangle {
-	public static void main(String[]){
-		MatrixTriangle matrix = MatrixTriangle;
-		int n = 6;
-		int[][] array = matrix.matrixTri(n) ;
+	public static void main(String[] arr) {
+		MatrixTriangle matrix = new MatrixTriangle();
+		int n = 10;
+		int[][] array = matrix.matrixTri(n);
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(Arrays.toString(array[i]));
 		}
-    }
+	}
 
 	public int[][] matrixTri(int n) {
-		int [][] arr = new int[n][n];
+		int[][] arr = new int[n][];
 		for (int i = 0; i < n; i++) {
-			for (int j = i; j < n; j++) {
-				a[i][j]=n;
-				System.out.print(" "+a[i][j]+" ");
+			int[] arrN = new int[n - i];
+			for (int j = 0; j < n - i; j++) {
+				arrN[j] = i + 1 + j;
 			}
-			System.out.println("");
-	}
+			arr[i] = arrN;
+		}
 		return arr;
+	}
 }
