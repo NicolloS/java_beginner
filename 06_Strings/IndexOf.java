484fa03d06de0a7477bcb74f25495ef05a12ac23
Anton Smolich <a.smolich@gmail.com>
2017-03-06 14:40:43 -0800

    Added string IndexOf problem

---
+++ 06_Strings/IndexOf.java
@@ -0,0 +1,10 @@
+/**
+ * <p>Index Of.</p>
+ *
+ * <p>Given a string S and substring to search SS.</p>
+ * <p>Return the index of the first occurrence of substring SS in string S, or -1 if SS is not part of S.</p>
+ */
+public class IndexOf {
+    public int indexOf(String str, String sub) {
+    }
+}
